9ed0808d1609a22028cb7783d027fc0f741c0563
YangYang <yysunny2006@gmail.com>
2018-11-15 15:13:18 +0800

    study webpack asset management

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
+function component() {
+    let element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    //Add the image to our existing div
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
+    return element;
+}
+
+document.body.appendChild(component());
